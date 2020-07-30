1ecdfb455cf6dbb980c6634985ac8b4fb69a5219
csergey8 <c.sergey@protonmail.com>
2020-07-30 10:17:23 +0300

    full functionalty

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import moment from 'moment';
+import { MonthItem } from './components/MonthItem';
+import './App.css';
+import List from '@material-ui/core/List';
+
+
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      months: moment.months(),
+      users: null
+    }
+  }
+  getUsers = async () => {
+    try {
+      let response = await fetch('https://yalantis-react-school-api.yalantis.com/api/task0/users')
+      const users = await response.json()
+      this.setState({
+        users
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  componentDidMount() {
+    this.getUsers()
+  }
+  render(){
+    return (
+      <div className="App">
+        <List>
+          {this.state.months.map(month => {
+            const users = this.state.users && this.state.users.filter(user => moment(user.dob).format('MMMM') === month)
+            return <MonthItem key={month} name={month} users={users}/>
+          })}
+        </List>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MonthItem.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import List from '@material-ui/core/List';
+import moment from 'moment';
+
+
+const MonthItem = ({name, users}) => {
+    const [open, setOpen] = React.useState(false)
+
+    let color;
+
+    if(users){
+        switch (true) {
+            case (users.length <= 2):
+                color = '9e9e9e'
+                break;
+            case (users.length <= 6):
+                color = '#2196f3'
+                break;
+            case (users.length <= 10):
+                color = '#4caf50'
+                break;
+            case (users.length >= 11):
+                color = '#f44336'
+                break;
+        }
+    }
+
+    return (
+        <React.Fragment>
+            <ListItem 
+                onMouseEnter={() => setOpen(true)}
+                onMouseLeave={() => setOpen(false)} 
+                button 
+                style={{
+                backgroundColor: color
+            }}>
+                <ListItemText primary={name}/>
+            </ListItem>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                {users && users.map(user => (
+                    <ListItem button>
+                    <ListItemText primary={`${user.firstName} ${user.lastName} - ${moment(user.dob).format('DD, MMM, YYYY')}`} />
+                    </ListItem>
+                ))}
+                </List>
+            </Collapse>
+        </React.Fragment>
+    )
+}
+
+export { MonthItem }
